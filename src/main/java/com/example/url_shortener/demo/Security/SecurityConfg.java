9214d11f24db3905efd16514f874f8972dad3802
eshika09 <eshika.bhatia886@gmail.com>
2026-06-10 16:30:18 +0530

    update security config and application properties

--- src/main/java/com/example/url_shortener/demo/Security/SecurityConfg.java
+++ src/main/java/com/example/url_shortener/demo/Security/SecurityConfg.java
@@ -4,6 +4,7 @@ import com.example.url_shortener.demo.Security.Jwt.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -31,6 +32,7 @@ public class SecurityConfg {
     public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
         http.csrf(csrf->csrf.disable())
                 .authorizeHttpRequests(auth->auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/urls/**").authenticated()
                         .requestMatchers("/{shorturl}").permitAll()
